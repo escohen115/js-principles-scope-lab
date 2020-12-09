66f367e98f86d41734f98875cd3c076962aaebeb
escohen115 <escohen115@gmail.com>
2020-12-09 17:34:04 -0500

    commit tree d77417cb3c07dc6e646b60097675e14c127336f3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+
+
+
+var customerName = 'bob'
+
+upperCaseCustomerName(){
+    customerName.charAt(0).toUpperCase() + name.slice(1)
+}
+
+setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
+
+
+overwriteBestCustomer(){
+    var bestCustomer = 'bob'
+}
+
+
+const leastFavoriteCustomer = 'a'
+
+
+changeLeastFavoriteCustomer(){
+
+leastFavoriteCustomer = 'b'
+
+}
